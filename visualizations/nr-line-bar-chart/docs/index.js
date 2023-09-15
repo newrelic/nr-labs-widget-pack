52273ec483462147218adff170c6582ebcfb0ec5
Kav91 <kav.pather@gmail.com>
2023-09-15 21:17:13 +1000

    feat: update docs

--- visualizations/nr-line-bar-chart/docs/index.js
+++ visualizations/nr-line-bar-chart/docs/index.js
@@ -95,6 +95,37 @@ export default function Docs() {
     <div style={{ textAlign: 'left' }}>
       <HeadingText type={HeadingText.TYPE.HEADING_2}>Documentation</HeadingText>
       <Card collapsible>
+        <CardHeader title="Overview" />
+        <CardBody style={{ marginLeft: '35px' }}>
+          <BlockText spacingType={[BlockText.SPACING_TYPE.MEDIUM]}>
+            Use the Line & Bar chart to understand changes in quantity values
+            (rendered as bars) alongside trends over time (rendered as lines).
+            For instance, you may be interested in understanding how
+            infrastructure load is impacted by throughput on your web site. Or
+            you may want to see if web page response time effects the total
+            number of orders processed - these are perfect use cases for a Line
+            & Bar Chart.
+            <br />
+            <br />
+            The Line & Bar chart plots data across three axes:
+            <Spacing type={[Spacing.TYPE.MEDIUM, Spacing.TYPE.EXTRA_LARGE]}>
+              <ul>
+                <li>the X axis represents time</li>
+                <li>
+                  the left Y axis represents the values for the Line Charts
+                </li>
+                <li>
+                  the right Y axis represents the values for the Bar Charts
+                </li>
+              </ul>
+            </Spacing>
+            The chart allows you to define multiple line and bar queries, so it
+            is highly recommended that the queries are aligned in terms of units
+            and time periods.
+          </BlockText>
+        </CardBody>
+      </Card>
+      <Card collapsible defaultCollapsed>
         <CardHeader title="Query Syntax" />
         <CardBody style={{ marginLeft: '35px' }}>
           <BlockText spacingType={[BlockText.SPACING_TYPE.MEDIUM]}>
@@ -134,11 +165,27 @@ export default function Docs() {
                 last week
               </code>
             </Spacing>
+            <br />
+            <br />
+            To include multiple lines and/or bars in the chart, you can either:
+            <Spacing type={[Spacing.TYPE.MEDIUM, Spacing.TYPE.EXTRA_LARGE]}>
+              <ul>
+                <li>
+                  define additional queries following the guidelines above
+                </li>
+                <li>
+                  include a FACET clause in your query. Note that if you are
+                  faceting on the same attribute in both the line and the bar
+                  queries, you will need to alias one of those facets in order
+                  to avoid name collisions in the chart output.
+                </li>
+              </ul>
+            </Spacing>
           </BlockText>
         </CardBody>
       </Card>
 
-      <Card collapsible>
+      <Card collapsible defaultCollapsed>
         <CardHeader title="Properties" />
         <CardBody style={{ marginLeft: '35px' }}>
           {properties.configuration
